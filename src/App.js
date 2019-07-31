d1bd0a510ba206f346a95f4bc93a54ac8dc56cfb
tronxi <sergio.gsanchez97@gmail.com>
2019-07-31 15:51:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Sergio',
+  lastName: 'Garcia'
+};
+function Componente(props) {
+  return (
+    <div>
+      Componente
+      <p className="nombre">Nombre {props.nombre} </p>
+      <p className="apellido">Apellidos {props.apellido}</p>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Componente nombre="sergio" apellido={user.lastName}/>
+        {formatName(user)}
       </header>
     </div>
   );
